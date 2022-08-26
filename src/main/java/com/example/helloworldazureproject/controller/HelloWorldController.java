c03f6455d707a7db4d13b274006a943e297a58f3
simonpedersen <simonorskov@gmail.com>
2022-08-26 09:23:33 +0200

    thymeleaf

--- src/main/java/com/example/helloworldazureproject/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworldazureproject/controller/HelloWorldController.java
@@ -1,2 +1,15 @@
-package com.example.helloworldazureproject.controller;public class HelloWorldController {
+package com.example.helloworldazureproject.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String getHelloWorld(){
+        return "index";
+    }
 }
